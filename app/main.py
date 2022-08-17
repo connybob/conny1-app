e210d1a5e0b27496612220cdc848327d291a7f18
connybee <connybee123@gmail.com>
2022-08-17 09:44:30 -0700

    Create first version

--- app/main.py
+++ app/main.py
@@ -9,7 +9,3 @@ def index():
 @app.route('/pageone')
 def pageone():
     return render_template("pageone.html")
-
-
-#host 0.0.0.0 makes it run on the network and the port 5000 is what port is open on my pc
-app.run(host="0.0.0.0", port=25565, debug=True)
